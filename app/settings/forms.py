d7790d444d836d05d5f00aba4460fc6dbbc38ff6
evgenyPro <evgeny_net_x@protonmail.com>
2021-04-30 02:07:28 +0700

    Implemented the sidebar of settings page [iss8989]
    This page is template for all settings pages. It contains sidebar with all tabs of settings

--- app/settings/forms.py
+++ app/settings/forms.py
@@ -1,9 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField
 from flask_wtf.file import FileField
 
 class EditSettingsForm(FlaskForm):
 	username = StringField('Username')
+	about_me = StringField('About me')
 	first_name = StringField('First name')
 	last_name = StringField('Last name')
 	email = StringField('Email')
